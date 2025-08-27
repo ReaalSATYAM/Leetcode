94220dd7231a7c1ed87fa20880d68473b223224e
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-08-27 20:22:28 +0530

    Update Pascals_Triangle.cpp

--- Pascals_Triangle.cpp
+++ Pascals_Triangle.cpp
@@ -1,12 +1,25 @@
 // Given row number r and column number c. Print the element at position (r, c) in Pascal’s triangle.
 int main() {
-    int n = 3, r = 2;
-    int res = 1;
+    int n = 3, r = 2;// For zero based indexing, decrease 'n' and 'r' by one(n-1, r-1)!
+    long long res = 1;
     for(int i = 0; i < r; i++){
         res = res*(n-i);
         res = res/(i+1);
     }
-    cout<<res;
+    cout<<res; // 3
+    return 0;
+}
+
+// Given the row number n. Print the n-th row of Pascal’s triangle.
+int main() {
+    int n = 10;// this will print the 11th row
+    long long res = 1;
+    cout<<res<<" ";
+    for(int i = 0; i < n; i++){
+        res = res*(n-i);
+        res = res/(i+1);
+        cout<<res<<" ";
+    }
     return 0;
 }
 
