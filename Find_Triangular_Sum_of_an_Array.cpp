a27c18b4f4ebfe38642fb77504c6d4f1dbf0790a
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-08-27 21:29:29 +0530

    Create Find_Triangular_Sum_of_an_Array.cpp

---
+++ Find_Triangular_Sum_of_an_Array.cpp
@@ -0,0 +1,15 @@
+// Time complexity O(n^2)
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        int sum = 0;
+        if(nums.size()== 1) return nums[0];
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n - i - 1; j++){
+                nums[j] = (nums[j] + nums[j+1]) % 10;
+            }
+        }
+        return nums[0]; 
+    }
+};
