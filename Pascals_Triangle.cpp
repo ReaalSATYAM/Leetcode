b3ad6b67e50901dca49e1bda9d783a1b9873e2f9
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-08-27 20:56:12 +0530

    Update Pascals_Triangle.cpp

--- Pascals_Triangle.cpp
+++ Pascals_Triangle.cpp
@@ -23,3 +23,45 @@ int main() {
     return 0;
 }
 
+// Given the number of rows n. Print the first n rows of Pascal’s triangle.
+//Leetcode solution
+class Solution {
+private:
+    vector<int> generateRow(int row){
+        long long res = 1;
+        vector<int> temp;
+        temp.push_back(1);
+        for(int col = 0; col < row; col++){
+            res = res*(row-col);
+            res = res/(col+1);
+            temp.push_back(res);
+        }
+        return temp;
+    }
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> ans;
+        for(int i = 0; i < n; i++){
+            ans.push_back(generateRow(i));
+        }
+        return ans;
+    }
+
+};
+
+//Easier version
+int main() {
+    int n = 10;
+    
+    for(int i = 0; i <= n; i++){
+        long long res = 1;
+        cout<<res<<" ";
+        for(int j = 0; j< i; j++){
+            res = res*(i-j);
+            res = res/(j+1);
+            cout<<res<<" ";
+        }
+        cout<<'\n';
+    }
+    return 0;
+}
