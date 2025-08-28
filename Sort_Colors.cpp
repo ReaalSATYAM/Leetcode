beec82f3db6297f881b4c8efc93214d84fe1dd60
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-08-28 21:39:03 +0530

    Create Sort_Colors.cpp

---
+++ Sort_Colors.cpp
@@ -0,0 +1,19 @@
+//Dutch National flag algorithm. 
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int low = 0, mid = 0, high = arr.size()-1; 
+        while(mid <= high){
+            if(arr[mid] == 0){
+                swap(arr[low], arr[mid]);
+                low++;
+                mid++;
+            } 
+            else if(arr[mid] == 2){
+                swap(arr[high],arr[mid]);
+                high--;
+            }
+            else mid++;
+        }
+    }
+};
