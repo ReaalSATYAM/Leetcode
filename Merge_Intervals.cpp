802c1aa33861cd8cf7dad78f0f22c51b2b7dfe43
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-08-29 23:17:29 +0530

    Update Merge_Intervals.cpp

--- Merge_Intervals.cpp
+++ Merge_Intervals.cpp
@@ -4,9 +4,11 @@ public:
         vector<vector<int>> ans;
         sort(arr.begin(), arr.end());
         for(int i = 0; i< arr.size(); i++){
+            // Skpping mearging
             if(ans.empty() || arr[i][0] > ans.back()[1] ){
                 ans.push_back(arr[i]);
             }
+            // Merging intervlas 
             else{
                 ans.back()[1] = max(ans.back()[1], arr[i][1]);
             }
