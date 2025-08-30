e86feb7b9fb1fa62bb5b60f678c9bdc61c077480
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-08-30 21:32:07 +0530

    Update Merge_Sorted_Array.cpp

--- Merge_Sorted_Array.cpp
+++ Merge_Sorted_Array.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i = m-1, j = n-1, k = m+n-1;
+        // placing the largest element at the end 
         while(i >= 0 && j >= 0) {
             nums1[k--] = nums1[i] >= nums2[j]? nums1[i--]: nums2[j--];
         }
