9459f299cbcb24e62e5752dd1ab016ac55d0fc59
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-02 11:27:43 +0530

    Update 3027. Find the Number of Ways to Place People II.cpp

--- 3027. Find the Number of Ways to Place People II.cpp
+++ 3027. Find the Number of Ways to Place People II.cpp
@@ -10,12 +10,14 @@ public:
         sort(points.begin(), points.end(), cmp);
 
         int ans = 0;
-        for(int i = 0; i< n; i++){
+        for(int i = 0; i < n; i++){
             int cx = points[i][0], cy = points[i][1];
             long k = INT_MIN;
             for(int j = i + 1; j< n; j++){
                 int tx = points[j][0], ty = points[j][1];
-                if(ty> cy) continue;
+                // continue if ty is heigher than cy 
+                if(ty > cy) continue;
+                // Check if the y cordinate is within the threshold value
                 if(ty > k){
                     ans++;
                     k = ty;
