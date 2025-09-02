0e2f6fa2cc4d4065d733d0fd0f67eb58c1ec916b
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-02 11:17:43 +0530

    Create  Code Code Sample Code Sample   Testcase Testcase Test Result 3025. Find the Number of Ways to Place People I.cpp

---
+++ Code Code Sample Code Sample   Testcase Testcase Test Result 3025. Find the Number of Ways to Place People I.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int n = points.size();
+        auto cmp = [&](vector<int> &c, vector<int> &t){
+            if(c[0] == t[0]) return c[1] > t[1];
+            return c[0] < t[0];
+        };
+        sort(points.begin(), points.end(), cmp);
+
+        int ans = 0;
+        for(int i = 0; i< n; i++){
+            int cx = points[i][0], cy = points[i][1];
+            int k = -1e9;
+            for(int j = i + 1; j< n; j++){
+                int tx = points[j][0], ty = points[j][1];
+                if(ty> cy) continue;
+                if(ty > k){
+                    ans++;
+                    k = ty;
+                }
+            }
+        }
+        return ans;
+    }
+};
