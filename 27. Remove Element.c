b8851f5a55a5b17328aa220ae2978666de3c867f
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-03 22:14:01 +0530

    Create 27. Remove Element.c

---
+++ 27. Remove Element.c
@@ -0,0 +1,35 @@
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int removeElement(int* nums, int n, int val) {
+    int c = 0, i = 0, j = n - 1, f;
+    if(n == 0)
+        return 0;
+    while(i <= j)
+    {
+        f = 1;
+        if(nums[i] == val)
+        {
+            if(nums[j] != val)
+            {
+                swap(&nums[i], &nums[j]);
+                c++;
+                j--;
+                f = 1;
+            }
+            else
+            {
+                j--;
+                f = 0;
+            }
+                
+        } 
+        if(f)
+            i++; 
+    }
+    return j + 1;
+}
