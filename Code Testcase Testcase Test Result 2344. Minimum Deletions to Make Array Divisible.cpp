049a6fd21e9662e4ac5207623919ae612535b60a
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-04 21:34:30 +0530

    Create  Code Testcase Testcase Test Result 2344. Minimum Deletions to Make Array Divisible.cpp

---
+++ Code Testcase Testcase Test Result 2344. Minimum Deletions to Make Array Divisible.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& numsDivide) {
+        int del_count = 0;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size(); i++){
+            int flag = 0;
+            for(int j = 0; j< numsDivide.size(); j++){
+                if(numsDivide[j] % nums[i] != 0){
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if(flag){
+                int temp = nums[i]; 
+                del_count++;
+            }
+            else return del_count;
+        }
+        return -1;
+    }
+};
