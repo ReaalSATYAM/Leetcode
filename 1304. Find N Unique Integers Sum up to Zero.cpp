0589e7e90f57d5b0a6fdeab0e34e674ff7d5b1e7
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-07 12:04:31 +0530

    Create 1304. Find N Unique Integers Sum up to Zero.cpp

---
+++ 1304. Find N Unique Integers Sum up to Zero.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> ans;
+        for(int i = n/2; i >= -1* (n/2); i--){
+            if(n%2 == 0)
+                if(i == 0) continue;
+            ans.push_back(i);
+        }
+        return ans;
+    }
+};
