a33ed7f4c1978aa2eba67c3bd6290f9f462cfc2c
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-07 23:34:26 +0530

    Create 6. Zigzag Conversion.c

---
+++ 6. Zigzag Conversion.c
@@ -0,0 +1,20 @@
+char* convert(char* s, int numRows) {
+    if (numRows == 1) return s;
+    int n = strlen(s);
+    char* res = (char*)malloc((n + 1) * sizeof(char));
+    int index = 0;
+    int jump = (numRows - 1) * 2;
+    for (int i = 0; i < numRows; i++) {
+        for (int j = i; j < n; j += jump) {
+            res[index++] = s[j];
+            if (i > 0 && i < numRows - 1) {
+                int zigzag = j + jump - 2 * i;
+                if (zigzag < n) {
+                    res[index++] = s[zigzag];
+                }
+            }
+        }
+    }
+    res[index] = '\0';
+    return res;
+}
