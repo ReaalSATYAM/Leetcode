6e56d6ea36edec7d447967659046a9c4fddc7244
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-08 11:33:17 +0530

    Update 1317. Convert Integer to the Sum of Two No-Zero Integers.cpp

--- 1317. Convert Integer to the Sum of Two No-Zero Integers.cpp
+++ 1317. Convert Integer to the Sum of Two No-Zero Integers.cpp
@@ -1,5 +1,6 @@
 class Solution {
 private:
+    // checks if the given number have zero or not
     bool no_zero(int n){
         while(n > 0){
             if(n % 10 == 0) return false;
@@ -8,6 +9,7 @@ private:
         return true;
     }
 public:
+    // we are checking all the possible values of of a and b (no zero decimal values only)
     vector<int> getNoZeroIntegers(int n) {
        for(int i = 1; i <= (n+1)/2; i++){
             int pos_a = i;
