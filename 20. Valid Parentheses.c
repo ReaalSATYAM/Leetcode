dc46e8bb098b6b0991fada9f357bd300b7582776
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-09 23:06:42 +0530

    Create 20. Valid Parentheses.c

---
+++ 20. Valid Parentheses.c
@@ -0,0 +1,22 @@
+bool isValid(char* s) {
+    int n=strlen(s);
+    char stack [n];
+    int top=-1;
+  for(int i=0;i<strlen(s);i++){
+    if(s[i]=='('||s[i]=='{'||s[i]=='['){
+        stack[++top]=s[i];
+
+    }
+    else{
+        if(top==-1){
+        return false;
+    }
+    char last=stack[top--];
+    if(s[i]==')'&&last!='('||s[i]=='}'&&last!='{'||s[i]==']'&&last!='['){
+        return false;
+    }
+
+  }
+}
+return top==-1;
+}
