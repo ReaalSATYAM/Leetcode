fb653c5d1414a35d832ea2ebc932ea62b96bc3b5
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-12 20:55:25 +0530

    Create 3227. Vowels Game in a String.cpp

---
+++ 3227. Vowels Game in a String.cpp
@@ -0,0 +1,17 @@
+class Solution {
+private: 
+    bool isVowel(char c){
+        c = tolower(c);
+        if(c == 'a' || c == 'e'||c == 'i' || c == 'o'|| c == 'u') return true;
+        return false;
+    }
+public:
+    bool doesAliceWin(string s) {
+        int vowel = 0;
+        for(char &c: s){
+            if(isVowel(c)) vowel++;
+        }
+        if(vowel == 0) return false;
+        return vowel > 0;
+    }
+};
