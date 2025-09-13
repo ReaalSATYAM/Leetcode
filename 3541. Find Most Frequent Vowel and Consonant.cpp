e7977acb78b7140d49b06b87e84f3bca0ff26594
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-13 09:39:34 +0530

    Update 3541. Find Most Frequent Vowel and Consonant.cpp

--- 3541. Find Most Frequent Vowel and Consonant.cpp
+++ 3541. Find Most Frequent Vowel and Consonant.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     int maxFreqSum(string s) {
         int hash[26] = {0};
+        // finding frequency of each char in s.
         for(int i = 0; i < s.length(); i++){
             hash[s[i] - 'a']++;
         }
         int maxV = 0, maxC = 0;
+
+        // finding the max frequency of vowels and consonants 
         for(int i = 0; i < s.length(); i++){
             if(s[i] == 'a' || s[i] == 'e'||s[i] == 'i' || s[i] == 'o'|| s[i] == 'u'){
                 maxV = hash[s[i] - 'a'] > maxV? hash[s[i] - 'a']: maxV;
