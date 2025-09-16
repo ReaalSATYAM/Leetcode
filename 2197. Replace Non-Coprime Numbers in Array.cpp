f2685ec857b325e4f0cb451a01c18bc618194487
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-16 20:16:07 +0530

    Create 2197. Replace Non-Coprime Numbers in Array.cpp

---
+++ 2197. Replace Non-Coprime Numbers in Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        vector<int> res;
+        for(int i = 0; i < nums.size(); i++){
+            while(!res.empty()){
+                int prev = res.back();
+                int curr = nums[i];
+                int GCD = gcd(prev, curr);
+                if(GCD == 1) break;
+
+                res.pop_back();
+                int LCM = prev/GCD * curr;
+                nums[i] = LCM;
+            }
+            res.push_back(nums[i]);
+            
+        }
+        return res;
+    }
+};
