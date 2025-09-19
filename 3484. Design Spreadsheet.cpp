d8cf68d52f22066eb1821622091040ebcbce70da
ReaalSATYAM <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-19 21:48:38 +0530

    Create 3484. Design Spreadsheet.cpp

---
+++ 3484. Design Spreadsheet.cpp
@@ -0,0 +1,37 @@
+class Spreadsheet {
+public:
+    unordered_map<string, int> sheet;
+    Spreadsheet(int rows) {
+        
+    }
+    
+    void setCell(string cell, int value) {
+        sheet[cell] = value;
+    }
+    
+    void resetCell(string cell) {
+        sheet[cell] = 0;
+    }
+    int solve(string a){
+        if(isdigit(a[0])){
+            return stoi(a);
+        }
+        return sheet[a];
+    }
+    int getValue(string formula) {
+        string s = formula.substr(1);
+        int plusIndex = s.find('+');
+        string left = s.substr(0, plusIndex);
+        string right = s.substr(plusIndex+1);
+
+        return solve(left) + solve(right);
+    }
+};
+
+/**
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet* obj = new Spreadsheet(rows);
+ * obj->setCell(cell,value);
+ * obj->resetCell(cell);
+ * int param_3 = obj->getValue(formula);
+ */
