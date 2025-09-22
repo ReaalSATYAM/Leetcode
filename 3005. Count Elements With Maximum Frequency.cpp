fc82b9b88f3468fd5c69244d3bbbd250a867aed0
Satyam Naithani <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-22 22:47:33 +0530

    Update 3005. Count Elements With Maximum Frequency.cpp

--- 3005. Count Elements With Maximum Frequency.cpp
+++ 3005. Count Elements With Maximum Frequency.cpp
@@ -3,6 +3,7 @@ public:
     int maxFrequencyElements(vector<int>& nums) {
         unordered_map<int, int> freqTable;
         int MAX = INT_MIN;
+        // filling frequency table. Also finding the max frequency.
         for(int it: nums){
             freqTable[it]++;
             if(freqTable[it] > MAX){
