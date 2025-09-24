50919370e1c780ddfe4e3152651db83f24174f96
Satyam Naithani <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-24 23:35:55 +0530

    Update 166. Fraction to Recurring Decimal.cpp

--- 166. Fraction to Recurring Decimal.cpp
+++ 166. Fraction to Recurring Decimal.cpp
@@ -4,22 +4,20 @@ public:
     string fractionToDecimal(int numerator, int denominator) {
         if(numerator == 0)
             return "0";
-
         string result;
 
         // for handeling negative numbers
         if((long long)numerator * (long long)denominator < 0) {
             result += "-";
         }
-
+        
         long long absNumerator = labs(numerator);
         long long absDenominator = labs(denominator);
-
         long long integerDiv = absNumerator / absDenominator;
-
-        result += to_string(integerDiv); //log10(integerDiv)
+        result += to_string(integerDiv); 
 
         long long remain = absNumerator % absDenominator;
+        // if the remainder is zero than return thr result else move to to the decimal part
         if(remain == 0) {
             return result;
         }
