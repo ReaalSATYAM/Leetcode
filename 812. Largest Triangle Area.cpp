d0067c86049b19d70de0b35dcf427f8829cbf5f3
Satyam Naithani <149092305+ReaalSATYAM@users.noreply.github.com>
2025-09-27 20:15:30 +0530

    Update 812. Largest Triangle Area.cpp

--- 812. Largest Triangle Area.cpp
+++ 812. Largest Triangle Area.cpp
@@ -19,3 +19,11 @@ public:
         return area;
     }
 };
+
+for(int i = 0; i < n - 2; i++){
+    for(int j = i + 1; j < n - 1; j++){
+        for(int k = j + 1; k < n; k++){
+            area = max(area, triangleArea(points[i], points[j], points[k]));
+        }
+    }
+}
