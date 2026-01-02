763665e104be76dab0a54c03469edba3d4170a9e
Satyam Naithani <149092305+ReaalSATYAM@users.noreply.github.com>
2026-01-02 23:31:34 +0530

    Update 961. N-Repeated Element in Size 2N Array.cpp

--- 961. N-Repeated Element in Size 2N Array.cpp
+++ 961. N-Repeated Element in Size 2N Array.cpp
@@ -18,3 +18,17 @@ public:
         return 0;
     }
 };
+
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums){
+        unordered_set<int> seen;
+        for(int i = 0; i < nums.size(); i++){
+            if(seen.count(nums[i])){
+                return nums[i];
+            }
+            seen.insert(nums[i]);
+        }
+        return 0;
+    }
+};
