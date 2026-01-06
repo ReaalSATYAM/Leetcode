4c63dcbd36cf9e91ba7adbe6ac6341f69eeb4fde
Satyam Naithani <149092305+ReaalSATYAM@users.noreply.github.com>
2026-01-06 23:13:13 +0530

    Update 1161. Maximum Level Sum of a Binary Tree.cpp

--- 1161. Maximum Level Sum of a Binary Tree.cpp
+++ 1161. Maximum Level Sum of a Binary Tree.cpp
@@ -2,6 +2,17 @@
 Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
 Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
