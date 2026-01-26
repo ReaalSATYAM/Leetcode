200eba38b99576176c6f4b49bb825737e17f7a96
Satyam Naithani <149092305+ReaalSATYAM@users.noreply.github.com>
2026-01-26 19:27:17 +0530

    Update 1200. Minimum Absolute Difference.cpp

--- 1200. Minimum Absolute Difference.cpp
+++ 1200. Minimum Absolute Difference.cpp
@@ -1,4 +1,10 @@
-
+/*
+Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements.
+Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+a, b are from arr
+a < b
+b - a equals to the minimum absolute difference of any two elements in arr
+*/
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
